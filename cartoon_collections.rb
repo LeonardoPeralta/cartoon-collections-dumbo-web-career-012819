decaefdf5c0925d977530d10d0ff64a91865bc08
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-19 04:44:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.collect {|x| x.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)# code an argument here
+  words.any? {|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
